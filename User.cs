e2d085ac69c35ffe31ca456ec14b3ea5147bb8de
Kimiko Muwanguzi <kimikomu@gmail.com>
2024-09-01 19:52:02 -0400

    change user to a record

--- User.cs
+++ User.cs
@@ -1,11 +1,27 @@
+using System.Collections.Concurrent;
+
 namespace MinimalAPIs;
 
-public class User
+public record User(int UserId, string Name, List<Todo>? Todos)
 {
-    public int UserId { get; set; }
+    public int UserId { get; set; } = UserId;
+
+    public string Name { get; set; } = Name;
+
+    public List<Todo>? Todos { get; set; } = Todos;
     
-    public string Name { get; set; }
+    public static readonly ConcurrentDictionary<int, User> All = new();
     
-    public List<Todo> Todos { get; set; }
+    static User()
+    {
+        All.TryAdd(1, new User(1, "Mike", [
+            new Todo(Guid.NewGuid(), "10", "Buy groceries"), 
+            new Todo(Guid.NewGuid(), "11", "Art")
+        ]));
+        All.TryAdd(2, new User(2, "Max", [
+            new Todo(Guid.NewGuid(), "12", "What the Heck"), 
+            new Todo(Guid.NewGuid(), "13", "Vacuum")
+        ]));
+    }
 }
 
