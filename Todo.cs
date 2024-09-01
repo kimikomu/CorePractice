3b5aeabdf342716955ebf801350f4d64f653da4d
Kimiko Muwanguzi <kimikomu@gmail.com>
2024-09-01 19:51:43 -0400

    Combine todo and todoItem into 1 record

--- Todo.cs
+++ Todo.cs
@@ -1,12 +1,27 @@
+using System.Collections.Concurrent;
+
 namespace MinimalAPIs;
 
-public class Todo
+public record Todo(Guid TodoId, string Id, string Title)
 {
-    public Guid TodoId { get; set; }
+    public string Id = Id;
+
+    public Guid TodoId { get; set; } = TodoId;
+
+    public string Title { get; set; } = Title;
+
+    public string? Status { get; set; }
     
-    public string Title { get; set; }
+    public string? Description { get; set; }
+
+    public List<string>? Categories { get; set; }
     
-    public string Status { get; set; }
+    public static readonly ConcurrentDictionary<string, Todo> All = new();
     
-    public List<string> Categories { get; set; }
+    static Todo()
+    {
+        All.TryAdd("1", new Todo(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afb1"), "3fa85f64-5717-4562-b3fc-2c963f66afb1", "Learn Minimal APIs"));
+        All.TryAdd("2", new Todo(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afb2"), "3fa85f64-5717-4562-b3fc-2c963f66afb2", "Build a sample project"));
+        All.TryAdd("3", new Todo(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afb3"),"3fa85f64-5717-4562-b3fc-2c963f66afb3", "Write Documentation"));
+    }
 }
